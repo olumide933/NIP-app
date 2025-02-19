aa40ae13899f883acad8297f16e304599ebba36a
olumide933 <falowoolumide@gmail.com>
2025-02-19 01:34:43 -0800

    my addons

--- src/pages/company/Company.jsx
+++ src/pages/company/Company.jsx
@@ -1,7 +1,8 @@
 const Company = ()=> {
     return(
         <div>
-            <h1>This is the company Component</h1>
+            <h1 style={{color:"blue"}}>This is the company Component</h1>
+            <p><b>We welcome you our users to our company page and we will be happy working with you in the future</b></p>
         </div>
     )
 }
